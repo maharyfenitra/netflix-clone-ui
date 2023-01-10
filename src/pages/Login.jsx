fca64266078f9b3495c8eac838bf6ebc37d434fe
Mahary R <maharyfenitra@gmail.com>
2023-01-10 18:02:33 +0300

    SignUp

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,95 @@
-import React from 'react'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import BackgroundImage from '../components/BackgroundImage';
+import Header from '../components/Header';
+import { firebaseAuth } from '../utils/firebase-config';
+import { onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom'
 
 export default function Login() {
+  const navigate = useNavigate();
+  const [showPassword, setShowPasseWord] = useState(false);
+  const [formValues, setFormValues] = useState({
+    email: "",
+    password: "",
+  })
+  const handleChange = (e) => setFormValues({ ...formValues, [e.target.name]: e.target.value });
+  const handleSignIn = async () => {
+    try{
+      const  {email, password} = formValues;
+      await signInWithEmailAndPassword(firebaseAuth, email, password);
+    } catch (err){
+      console.log(err)
+    }
+  }
+
+  onAuthStateChanged(firebaseAuth, (currentUser)=>{
+    if(currentUser){
+      navigate("/");
+    } 
+  })
   return (
-    <div>Login</div>
+    <Container showPassword={showPassword}>
+      
+    </Container>
   )
 }
+
+const Container = styled.div`
+  position: relative;
+  .content{
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0,0,0,0.5);
+    height: 100vh;
+    width: 100vw;
+    display: grid;
+    grid-template-rows:15vh 85vh;
+    .body {
+      gap: 1rem;
+      .text {
+        gap: 1rem;
+        text-align: center;
+        font-size: 2rem;
+        h1 {
+          padding: 0 25rem;
+        }
+      }
+      .form {
+        display: grid;
+        grid-template-columns: ${({ showPassword }) => (showPassword ? "1fr 1fr" : "2fr 1fr")};
+        width: 60%;
+        input {
+          color: black;
+          border: none;
+          padding: 1.5rem;
+          font-size: 1.2rem;
+          border: 1px solid black;
+          &:focus {
+            outline: none;
+          }
+        }
+        button {
+          padding: 0.5rem 1rem;
+          background-color: #e50914;
+          border: none;
+          cursor: pointer;
+          color: white;
+          font-weight: bolder;
+          font-size: 1.05rem;
+        }
+      }
+      button {
+        padding: 0.5rem 1rem;
+        background-color: #e50914;
+        border: none;
+        cursor: pointer;
+        color: white;
+        border-radius:0.2rem;
+        font-weight: bolder;
+        font-size: 1.05rem;
+      }
+    }
+  }
+`
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import BackgroundImage from '../components/BackgroundImage';
 import Header from '../components/Header';
+import { firebaseAuth } from '../utils/firebase-config';
+import { createUserWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom'
 
 export default function Signup() {
+  const navigate = useNavigate();
+  const [showPassword, setShowPasseWord] = useState(false);
+  const [formValues, setFormValues] = useState({
+    email: "",
+    password: "",
+  })
+  const handleChange = (e) => setFormValues({ ...formValues, [e.target.name]: e.target.value });
+  const handleSignIn = async () => {
+    try{
+      const  {email, password} = formValues;
+      await createUserWithEmailAndPassword(firebaseAuth, email, password);
+    } catch (err){
+      console.log(err)
+    }
+  }
+
+  onAuthStateChanged(firebaseAuth, (currentUser)=>{
+    if(currentUser){
+      navigate("/");
+    } 
+  })
   return (
-    <Container>
-      <BackgroundImage/>
+    <Container showPassword={showPassword}>
+      <BackgroundImage />
       <div className="content">
-        <Header />
+        <Header login />
         <div className="body flex column a-center j-center">
           <div className="text flex column">
             <h1>Unlimited movies, TV shows and more</h1>
@@ -16,11 +40,16 @@ export default function Signup() {
             <h6>Ready to watch? Enter your email to create or restart membership</h6>
           </div>
           <div className="form">
-            <input type="email" placeholder="Email Address" name="email" />
-            <input type="password" placeholder="Password" name="password" />
-            <button>Get Started</button>
+            <input type="email" placeholder="Email Address" name="email" value={formValues.email} onChange={handleChange}/>
+            {
+              showPassword && <input type="password" placeholder="Password" name="password" value={formValues.password} onChange={handleChange} />
+            }
+            {
+              !showPassword && <button onClick={() => setShowPasseWord(true)}>Get Started</button>
+            }
+
           </div>
-          <button>Log in</button>
+          <button onClick={handleSignIn}>Log in</button>
         </div>
       </div>
     </Container>
@@ -38,6 +67,50 @@ const Container = styled.div`
     width: 100vw;
     display: grid;
     grid-template-rows:15vh 85vh;
-    
+    .body {
+      gap: 1rem;
+      .text {
+        gap: 1rem;
+        text-align: center;
+        font-size: 2rem;
+        h1 {
+          padding: 0 25rem;
+        }
+      }
+      .form {
+        display: grid;
+        grid-template-columns: ${({ showPassword }) => (showPassword ? "1fr 1fr" : "2fr 1fr")};
+        width: 60%;
+        input {
+          color: black;
+          border: none;
+          padding: 1.5rem;
+          font-size: 1.2rem;
+          border: 1px solid black;
+          &:focus {
+            outline: none;
+          }
+        }
+        button {
+          padding: 0.5rem 1rem;
+          background-color: #e50914;
+          border: none;
+          cursor: pointer;
+          color: white;
+          font-weight: bolder;
+          font-size: 1.05rem;
+        }
+      }
+      button {
+        padding: 0.5rem 1rem;
+        background-color: #e50914;
+        border: none;
+        cursor: pointer;
+        color: white;
+        border-radius:0.2rem;
+        font-weight: bolder;
+        font-size: 1.05rem;
+      }
+    }
   }
 `
